65dd3799d7c2397db2283ba5ab5dfeb50859aca5
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-07 22:11:17 +0300

    Start SberSchool new project

---
+++ src/main/java/org/nta/lessons/lesson1.java
@@ -0,0 +1,13 @@
+package org.nta.lessons;
+
+/**
+ * Hello world!
+ *
+ */
+public class lesson1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
