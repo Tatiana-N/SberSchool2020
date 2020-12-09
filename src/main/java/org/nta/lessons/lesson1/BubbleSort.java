e2945c2e65f886df58a6e1f1234d82ecad02e355
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-10 00:20:32 +0300

    Bubble sort for array with generics

--- src/main/java/org/nta/lessons/lesson1/BubbleSort.java
+++ src/main/java/org/nta/lessons/lesson1/BubbleSort.java
@@ -1,29 +1,34 @@
 package org.nta.lessons.lesson1;
 
-public class BubbleSort {
+public class BubbleSort<T extends Comparable> {
   public static void main(String[] args) {
     BubbleSort bSort = new BubbleSort();
-    int[] massive = {4, 3, 7, 6, 4, 8, 5, 0, 9, 1, 2};
-    bSort.sortMassive(massive);
-    for (int a : massive) {
+    Integer [] array1 = {4, 3, 7, 6, 4, 8, 5, 0, 9, 1, 2};
+    String [] array2 = {"sun","apple","in","row","pare","asia","knot"};
+    bSort.sortArray(array1);
+    bSort.sortArray(array2);
+    for (Integer a : array1) {
       System.out.print(a + " ");
-
+    }
+    System.out.println();
+    for (String a : array2) {
+      System.out.print(a + " ");
     }
   }
 
-  public int[] sortMassive(int[] massive) {
+  public T[] sortArray(T[] array) {
     boolean changed = true;
     while (changed) {
       changed = false;
-      for (int i = 1; i < massive.length; i++) {
-        if (massive[i - 1] > massive[i]) {
+      for (int i = 1; i < array.length; i++) {
+        if (array[i - 1].compareTo(array[i]) > 0) {
           changed = true;
-          int temp = massive[i - 1];
-          massive[i - 1] = massive[i];
-          massive[i] = temp;
+          T temp = array[i - 1];
+          array[i - 1] = array[i];
+          array[i] = temp;
         }
       }
     }
-    return massive;
+    return array;
   }
 }
