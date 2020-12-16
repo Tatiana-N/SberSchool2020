7e7bfcbdb669a3dff83acdd55469fe2644dda9d8
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-17 02:07:01 +0300

    Splitter of cars сплошной Stream

--- src/main/java/org/nta/lessons/lesson2/carsplitter/Splitter.java
+++ src/main/java/org/nta/lessons/lesson2/carsplitter/Splitter.java
@@ -3,24 +3,14 @@ package org.nta.lessons.lesson2.carsplitter;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Splitter {
-  public Map<String, List<Car>> split(List<Car> list) {
-
-    Set<String> types = new HashSet<>();
-    for (Car car:list) {
-      types.add(car.getType()); // list.stream().map(Car::getType).collect(Collectors.toSet()); замена
+  public class Splitter {
+    public Map<String, List<Car>> split(List<Car> list) {
+      return list.stream()
+        .map(Car::getType)
+        .collect(Collectors.toSet()).stream()
+        .collect(Collectors.toMap(
+                   p -> p,
+                   t -> list.stream()
+                     .collect(Collectors.partitioningBy(element -> element.getType().equals(t))).get(true)));
     }
-
-    Map<String, List<Car>> map = new HashMap<>();
-
-    for (String type : types) {
-      List<Car> listWithOneType = new ArrayList<>();
-      for (Car car:list) {
-        if (car.getType().equals(type)) {
-          listWithOneType.add(car); //list.stream().filter(car -> car.getType().equals(type)).collect(Collectors.toList())); замена
-        }
-      }
-      map.put(type, listWithOneType);}
-    return map;
   }
-}
