90d556fd8360a96f47fa626c2a9765839213dd8e
Tatiana-N <tanuskanikolaeva@mail.ru>
2020-12-17 00:40:53 +0300

    refactor of splitter of cars

--- src/main/java/org/nta/lessons/lesson2/carsplitter/Splitter.java
+++ src/main/java/org/nta/lessons/lesson2/carsplitter/Splitter.java
@@ -5,15 +5,22 @@ import java.util.stream.Collectors;
 
 public class Splitter {
   public Map<String, List<Car>> split(List<Car> list) {
-    Set<String> types = list.stream()
-                            .map(Car::getType)
-                            .collect(Collectors.toSet());
-    Map<String, List<Car>> map = new HashMap<>();
-    for (String type : types) {
-      map.put(type, list.stream()
-                        .filter(car -> car.getType().equals(type))
-                        .collect(Collectors.toList()));
+
+    Set<String> types = new HashSet<>();
+    for (Car car:list) {
+      types.add(car.getType()); // list.stream().map(Car::getType).collect(Collectors.toSet()); замена
     }
+
+    Map<String, List<Car>> map = new HashMap<>();
+
+    for (String type : types) {
+      List<Car> listWithOneType = new ArrayList<>();
+      for (Car car:list) {
+        if (car.getType().equals(type)) {
+          listWithOneType.add(car); //list.stream().filter(car -> car.getType().equals(type)).collect(Collectors.toList())); замена
+        }
+      }
+      map.put(type, listWithOneType);}
     return map;
   }
 }
