d0778ddd54bfd8bff8d15887e8f835d425b2a047
Tatiana-N <tanuskanikolaeva@mail.ru>
2021-01-31 20:35:05 +0300

    HW JMM begin

--- src/main/java/org/nta/lessons/lesson12/CallTask.java
+++ src/main/java/org/nta/lessons/lesson12/CallTask.java
@@ -4,14 +4,15 @@ import java.util.concurrent.Callable;
 
 public class CallTask<T> implements Callable<T> {
   T object;
+
   public CallTask(T object) {
     this.object = object;
   }
 
   @Override
   public T call() throws Exception {
-      System.out.println(Thread.currentThread().getName());
-      Thread.sleep(2000);
+    System.out.println(Thread.currentThread().getName());
+    Thread.sleep(2000);
     return object;
   }
 }
--- src/main/java/org/nta/lessons/lesson12/Task.java
+++ src/main/java/org/nta/lessons/lesson12/Task.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Callable;
 public class Task<T> {
   private static final String ANSI_PURPLE = "\u001B[35m";
   private static final String ANSI_RESET = "\u001B[0m";
- // static Object lock = new Object();
+  // static Object lock = new Object();
   private T value;
   Callable<? extends T> callable;
 
@@ -26,9 +26,10 @@ public class Task<T> {
           if (this.value == null) {
             try {
               this.value = temp = callable.call();
-          } catch (RuntimeException E){
+            } catch (RuntimeException E) {
               throw new RuntimeException("Новое");
-            }}
+            }
+          }
         }
       }
       return temp;
@@ -37,50 +38,48 @@ public class Task<T> {
 
   public static void main(String[] args) throws Exception {
     List<Integer> list = new ArrayList<>();
-    Thread thread1 = new Thread(() ->  {
-        Task<Integer> task = new Task<>(new CallTask<>(1));
-        try {
-          System.out.println(task.get());
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+    Thread thread1 = new Thread(() -> {
+      Task<Integer> task = new Task<>(new CallTask<>(1));
+      try {
+        System.out.println(task.get());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     });
     Thread thread2 = new Thread(() -> {
-        Task<Integer> task = new Task<>(new CallTask<>(2));
-        try {
-          System.out.println(task.get());
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+      Task<Integer> task = new Task<>(new CallTask<>(2));
+      try {
+        System.out.println(task.get());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     });
     Thread thread3 = new Thread(() -> {
-        Task<Integer> task = new Task<>(new CallTask<>(3));
-        try {
-          System.out.println(task.get());
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+      Task<Integer> task = new Task<>(new CallTask<>(3));
+      try {
+        System.out.println(task.get());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     });
     Thread serviceThread = new Thread(() -> {
       while (!Thread.interrupted()) {
-      System.out.println(ANSI_PURPLE + thread1.getState() +  " " + thread2.getState() + " " + thread3.getState());
-      System.out.println(ANSI_RESET);
-
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }}
+        System.out.println(ANSI_PURPLE + thread1.getState() + " " + thread2.getState() + " " + thread3.getState());
+        System.out.println(ANSI_RESET);
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
     });
     serviceThread.setDaemon(true);
     serviceThread.start();
     thread1.start();
     thread2.start();
     thread3.start();
-
-
     Thread.sleep(4000);
 
 
-}
+  }
 }
